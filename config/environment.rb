55afef9f31f6dca864e241c009f0244be05294fc
Chris McCormack <cmccormack@@users.noreply.github.com>
2017-01-03 14:58:46 +0000

    Updated mailer settings for Mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'guarded-fjord-80628.herokuapp.com/about',
+  :domain         => 'guarded-fjord-80628.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
