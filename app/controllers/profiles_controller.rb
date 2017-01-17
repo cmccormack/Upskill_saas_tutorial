698a78cd8bac8ec83b161d1ed3f104f451f10d70
Chris McCormack <cmccormack@@users.noreply.github.com>
2017-01-17 03:20:25 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,9 @@
-class ContactsController < ApplicationController
+class ProfilesController < ApplicationController
   
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
-    
+    @profile = Profile.new
   end
   
 end
