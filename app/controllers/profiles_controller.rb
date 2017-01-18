8d1e5428ef19d64d935986f68626ac7125633a37
Chris McCormack <cmccormack@@users.noreply.github.com>
2017-01-18 20:16:56 +0000

    Added user authentication requirement for viewing and editing profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
   
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
@@ -14,7 +17,8 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     if @profile.save
       flash[:success] = "Profile updated!"
-      redirect_to user_path( params[:user_id] )
+      # Redirect user to their profile page
+      redirect_to user_path( id: params[:user_id] )
     else
       render action: :new
     end
@@ -26,9 +30,29 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  # PATCH to /users/:user_id/profile
+  def update
+    # Retrieve user and user's profile from the database
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save (update)
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path( id: params[:user_id] )
+    else
+      render action :edit
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description )
     end
   
+    def only_current_user
+      @user = User.find ( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
+      
 end
