aca73cb44656bc47c1670aad5f6fcdf5a1169822
Chris McCormack <cmccormack@@users.noreply.github.com>
2017-01-20 18:41:15 +0000

    Added additional job titles and updated Forgot Password page with boostrap styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,6 +6,10 @@ module UsersHelper
       "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
       "<i class='fa fa-dollar'></i>".html_safe
+    elsif @user.profile.job_title == "Scientist"
+      "<i class='fa fa-flask'></i>".html_safe
+    elsif @user.profile.job_title == "Engineer"
+      "<i class='fa fa-pencil-square-o'></i>".html_safe
     end
   end
 end
